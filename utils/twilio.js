3b24aec8e2ccb1938e54d32fae540e33a13a0369
rishwankk <rishwan063@gmail.com>
2024-07-19 12:36:16 +0530

    error fixed

--- utils/twilio.js
+++ utils/twilio.js
@@ -3,30 +3,30 @@
 
 
 
-const twilio = require('twilio');
+// const twilio = require('twilio');
 
-const accountSid = process.env.ACCOUNT_SID;
-const authToken =process.env.ACCOUNT_TOKEN ;
-const client = twilio(accountSid, authToken);
+// const accountSid = process.env.ACCOUNT_SID;
+// const authToken =process.env.ACCOUNT_TOKEN ;
+// const client = twilio(accountSid, authToken);
 
-const sendOtp=async(to,otp)=>{
-    try {
-        const message = await client.messages.create({
-          body: `Your OTP code is ${otp}`,
-          from: '+16122940749',
-          to: `+91${to}`
-        });
-        console.log('OTP sent:', message.sid);
-        return true; // Indicate success
-      } catch (error) {
-        console.error('Error sending OTP:', error);
-        return false; // Indicate failure
-      }
-}
-
-
+// const sendOtp=async(to,otp)=>{
+//     try {
+//         const message = await client.messages.create({
+//           body: `Your OTP code is ${otp}`,
+//           from: '+16122940749',
+//           to: `+91${to}`
+//         });
+//         console.log('OTP sent:', message.sid);
+//         return true; // Indicate success
+//       } catch (error) {
+//         console.error('Error sending OTP:', error);
+//         return false; // Indicate failure
+//       }
+// }
 
 
-    module.exports={
-        sendOtp
-    }
+
+
+//     module.exports={
+//         sendOtp
+//     }
